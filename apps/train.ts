909c191efabe67b5781b2ddb3951525508cfdfc2
Shukhfayz <shukhfayz@gmail.com>
2024-05-09 06:05:17 -0400

    ZJ-TASK

---
+++ apps/train.ts
@@ -0,0 +1,12 @@
+// ZJ-TASK:
+
+// Shunday function yozing, u berilgan arrayni ichidagi numberlarni qiymatini hisoblab qaytarsin.
+// MASALAN: reduceNestedArray([1, [1, 2, [4]]]) return 8
+
+// function reduceNestedArray(param: (number | (number | number[])[])[]) {
+//     const res = param.flat()
+//     console.log(res)
+// }
+
+//reduceNestedArray([1, [1, 2, [4]]])
+

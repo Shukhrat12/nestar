f658086fcc57830c937323a2981af703d4603478
Shukhfayz <shukhfayz@gmail.com>
2024-05-19 22:49:39 -0400

    feat: develop getMember GraphQL API

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -7,6 +7,7 @@ import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
+import { T } from '../../libs/types/common';
 
 @Injectable()
 export class MemberService {
@@ -49,24 +50,32 @@ export class MemberService {
     }
 
     public async updateMember(
-        memberId: ObjectId, 
+        memberId: ObjectId,
         input: MemberUpdate): Promise<Member> {
         const result: Member = await this.memberModel.findOneAndUpdate(
-        {
-            _id: memberId, 
-            memberStatus: MemberStatus.ACTIVE
-        }, input, {new: true}
+            {
+                _id: memberId,
+                memberStatus: MemberStatus.ACTIVE
+            }, input, { new: true }
         ).exec();
 
         if (!result) throw new InternalServerErrorException(Message.UPLOAD_FAILED)
-        
+
         result.accessToken = await this.authService.createToken(result)
 
         return result;
     }
 
-    public async getMember(): Promise<string> {
-        return 'getMember executed'
+    public async getMember(targetId: ObjectId): Promise<Member> {
+        const search: T = {
+            _id: targetId,
+            memberStatus: {
+                $in: [MemberStatus.ACTIVE, MemberStatus.BLOCK]
+            }
+        }
+        const targetMember = this.memberModel.findOne(search).exec();
+        if (!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND)
+        return targetMember;
     }
 
     public async updateMemberByAdmin(): Promise<string> {

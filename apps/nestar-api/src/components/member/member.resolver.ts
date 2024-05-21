9b38797a81a24666a9af417eb9951eca8b6eba5d
Shukhfayz <shukhfayz@gmail.com>
2024-05-21 01:53:33 -0400

    feat: develop getAgents GraphQL API

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,7 +1,7 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { UseGuards } from '@nestjs/common';
-import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { AgentsInquiry, LoginInput, MemberInput, MembersInquiry } from '../../libs/dto/member/member.input';
 import { Member, Members } from '../../libs/dto/member/member';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
@@ -61,25 +61,25 @@ export class MemberResolver {
     public async getMember(@Args('memberId') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Member> {
         console.log('Query: getMember');
         const targetId = shapeIntoMongoObjectId(input)
-        return this.memberService.getMember(memberId, targetId);
+        return await this.memberService.getMember(memberId, targetId);
     }
 
     @UseGuards(WithoutGuard)
     @Query(() => Members)
     public async getAgents(@Args('input') input: AgentsInquiry, @AuthMember('_id') memberId: ObjectId): Promise<Members> {
         console.log('Query: getAgents');
-        return this.memberService.getAgents(memberId, input);
+        return await this.memberService.getAgents(memberId, input);
     }
 
     /** ADMIN **/
     @Roles(MemberType.ADMIN)
     @UseGuards(RolesGuard)
-    @Mutation(() => String)
-    public async getAllMembersByAdmin(): Promise<string> {
-        return this.memberService.getAllMembersByAdmin()
+    @Query(() => Members)
+    public async getAllMembersByAdmin(@Args('input') input: MembersInquiry): Promise<Members> {
+        return await this.memberService.getAllMembersByAdmin(input)
     }
 
-    // Authorization: ADMIN
+
     @Mutation(() => String)
     public async updateMemberByAdmin(): Promise<string> {
         return this.memberService.updateMemberByAdmin()

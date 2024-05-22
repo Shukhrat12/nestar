5a086b9acf9fd5e726f739c6e6c8b473b1031056
Shukhfayz <shukhfayz@gmail.com>
2024-05-22 00:13:48 -0400

    ZO-TASK

--- train.ts
+++ train.ts
@@ -1,46 +1,28 @@
-// ZK-TASK:
+// Shunday function yozing, u parametrdagi string ichidagi qavslar miqdori balansda ekanligini aniqlasin. 
+//Ya'ni ochish("(") va yopish(")") qavslar soni bir xil bolishi kerak.
+// MASALAN: areParenthesesBalanced("string()ichida(qavslar)soni()balansda") return true
 
-// Shunday function yozing, u har soniyada bir marta consolega 1 dan 5 gacha bolgan raqamlarni chop etsin va 5 soniyadan keyin ishini toxtatsin.
-// MASALAN: printNumbers()
-
-// function printNumbers() {
-//     for (let i = 0; i < 5; i++) {
-//         console.log(i)
-//     }
-// }
-
-// let res = setInterval(printNumbers, 1000)
-
-// setTimeout(() => { clearInterval(res); printNumbers }, 5000);
-
-// Shunday function yozing, 
-// u parametrda berilgan stringni kebab casega otkazib qaytarsin. 
-// Bosh harflarni kichik harflarga ham otkazsin.
-
-// MASALAN: stringToKebab(“I love Kebab”) return “i-love-kebab”
-
-// function stringToKebab(str: string) {
-//     const res = str.replaceAll(" ", "-").toLocaleLowerCase();
-//     return res
-// }
-
-// const result = stringToKebab("I love Kebab")
-// console.log(result)
-
-// ZM-TASK:
-
-// Shunday function yozing, u function parametrga berilgan raqamlarni orqasiga ogirib qaytarsin.
-// MASALAN: reverseInteger(123456789) return 987654321
-
-function reverseInteger(num: number) {
-    const strParam = num.toString()
-    let str = ''
-    for (let i = strParam.length - 1; i >= 0; i--) {
-        const element = strParam[i];
-        str += element
+function areParenthesesBalanced(input: string): boolean {
+    if (!input.includes('(') && !input.includes(')')) {
+        return false;
     }
-    return str
+    let balance = 0;
+    for (let char of input) {
+        if (char === '(') {
+            balance++;
+        } else if (char === ')') {
+            balance--;
+        }
+        if (balance < 0) {
+            return false;
+        }
+    }
+    return balance === 0;
 }
 
-const result = reverseInteger(123456789)
-console.log(result)
+console.log(areParenthesesBalanced("string()ichida(qavslar)soni()balansda")); // true
+console.log(areParenthesesBalanced("string)ichida(qavslar(")); // false
+console.log(areParenthesesBalanced("string(ichida(qavslar)soni()balansda(")); // false
+console.log(areParenthesesBalanced("((()))")); // true
+console.log(areParenthesesBalanced("(()))(")); // false
+console.log(areParenthesesBalanced("string")); // false

4c3cb356ca06e70bcc04f24fc9bc7fdf17d9fc44
Shukhfayz <shukhfayz@gmail.com>
2024-05-30 01:29:50 -0400

    feat: develop getComments GraphQL api

--- apps/nestar-api/src/comment/comment.resolver.ts
+++ apps/nestar-api/src/comment/comment.resolver.ts
@@ -1,13 +1,14 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../components/auth/guards/auth.guard';
-import { CommentInput } from '../libs/dto/comment/comment.input';
+import { CommentInput, CommentsInquiry } from '../libs/dto/comment/comment.input';
 import { AuthMember } from '../components/auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
-import { Comment } from '../libs/dto/comment/comment'
+import { Comment, Comments } from '../libs/dto/comment/comment'
 import { shapeIntoMongoObjectId } from '../libs/config';
 import { CommentUpdate } from '../libs/dto/comment/comment.update';
+import { WithoutGuard } from '../components/auth/guards/without.guard';
 
 @Resolver()
 export class CommentResolver {
@@ -25,12 +26,24 @@ export class CommentResolver {
 
     @UseGuards(AuthGuard)
     @Mutation(() => Comment)
-    public async updateComment (
+    public async updateComment(
         @Args('input') input: CommentUpdate,
         @AuthMember('_id') memberId: ObjectId): Promise<Comment> {
         console.log('Mutation: updateComment')
         input._id = shapeIntoMongoObjectId(input._id)
         return await this.commentService.updateComment(memberId, input)
+    }
+
+    @UseGuards(WithoutGuard)
+    @Query(() => Comments)
+    public async getComments(
+        @Args('input') input: CommentsInquiry,
+        @AuthMember('_id') memberId: ObjectId
+    ): Promise<Comments> {
+        console.log('Query: getComments')
+        input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
+        const result = await this.commentService.getComment(memberId, input);
+        return result;
     }
 
 }
--- apps/nestar-api/src/comment/comment.service.ts
+++ apps/nestar-api/src/comment/comment.service.ts
@@ -1,13 +1,16 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { CommentInput } from '../libs/dto/comment/comment.input';
+import { CommentInput, CommentsInquiry } from '../libs/dto/comment/comment.input';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
 import { MemberService } from '../components/member/member.service';
-import { Message } from '../libs/enums/common.enum';
+import { Direction, Message } from '../libs/enums/common.enum';
 import { PropertyService } from '../components/property/property.service';
 import { BoardArticleService } from '../components/board-article/board-article.service';
 import { CommentGroup, CommentStatus } from '../libs/enums/comment.enum';
 import { CommentUpdate } from '../libs/dto/comment/comment.update';
+import { T } from '../libs/types/common';
+import { lookupMember } from '../libs/config';
+import { Comments } from '../libs/dto/comment/comment';
 
 @Injectable()
 export class CommentService {
@@ -70,5 +73,34 @@ export class CommentService {
 
         if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED)
         return result;
+    }
+
+    public async getComment(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
+        const { commentRefId } = input.search
+        const match: T = {}
+        const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC }
+
+        const result: Comments[] = await this.commentModel
+            .aggregate([
+                { $match: match },
+                { $sort: sort },
+                {
+                    $facet: {
+                        list: [
+                            { $skip: (input.page - 1) * input.limit },
+                            { $limit: input.limit },
+                            lookupMember,
+                            {
+                                $unwind: '$memberData'
+                            }
+                        ],
+                        metaCounter: [{ $count: 'total' }]
+                    },
+                }
+
+            ])
+            .exec();
+        if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND)
+        return result[0];
     }
 }
